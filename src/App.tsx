4999867431f9ec6bce32e3a0f6af13f0a11bff01
heycn <heycn@foxmail.com>
2023-11-08 14:21:40 +0800

    add Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Icon } from '@/components/shared/icon'
+
 export const App: React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <div className="bg-pink-200 text-lg underline p-8 w-96 mt-8 m-auto rounded-lg">
       Hello world!
-    </h1>
+      <Icon className="w-[1.2em] h-[1.2em] text-blue-500" icon="mdi-light:home" />
+    </div>
   )
 }
