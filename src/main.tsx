7e6b17ba7c5dce329e3be90bbf6777df1ff1e0d9
heycn <heycn@foxmail.com>
2023-11-08 15:05:49 +0800

    add zustand as state management

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import '@/global.css'
+import { App } from './App'
 import { router } from '@/router/router'
 
 const div = document.getElementById('root')!
@@ -10,5 +11,6 @@ const app = ReactDOM.createRoot(div)
 app.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
