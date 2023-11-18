696b2245e87cec6a59bea90e8c129014120fa4b0
heycn <heycn@foxmail.com>
2023-11-18 11:05:24 +0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,17 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  stylistic: true,
+
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  typescript: true,
+  jsonc: false,
+  yaml: false,
+
   ignores: [
     '**/*.md',
   ],
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/global.css'
-import '@/app.css'
 import { App } from './App'
 
 const div = document.getElementById('root')!
 const app = ReactDOM.createRoot(div)
 
-app.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
-)
+app.render(<App />)
